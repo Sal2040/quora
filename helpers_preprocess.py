36c832b04da3259bc4f155066d56ff179c1088dd
Samer Alwan <alwan.samer24@gmail.com>
2023-09-08 21:13:41 +0200

    DatasetMaker created

--- helpers_preprocess.py
+++ helpers_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 
 class DataCleaner:
@@ -46,7 +47,8 @@ class DataCleaner:
         df = self._read_csv(input_path, train=True)
         X = df['question_text']
         y = df['target']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=23, shuffle=True, stratify=y)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=test_size, random_state=23, shuffle=True, stratify=y)
 
         df_train = pd.concat([X_train, y_train], axis=1)
         df_test = pd.concat([X_test, y_test], axis=1)
@@ -61,3 +63,27 @@ class DataCleaner:
         df_pos.to_csv(output_path[0], index=False)
         df_neg.to_csv(output_path[1], index=False)
         print("Pos/neg split successful.")
+
+class DatasetMaker:
+    def _cast_target(self, features, target):
+        target = tf.cast(target, tf.float32)  # cast target column to float32
+        return features, target
+
+    def make_dataset(self, input_path, batch_size, label_name="target", shuffle=True, buffer_size=60):
+        ds = tf.data.experimental.make_csv_dataset(
+            input_path,
+            batch_size=batch_size,
+            label_name=label_name,
+            shuffle=shuffle,
+            shuffle_buffer_size=buffer_size
+        )
+        return ds.map(self._cast_target)
+
+    def combine_datasets(self, pos_ds, neg_ds, batch_size, weights=None):
+        if weights is None:
+            weights = [0.5, 0.5]
+        pos_ds = pos_ds.unbatch()
+        neg_ds = neg_ds.unbatch()
+        resampled_ds = tf.data.Dataset.sample_from_datasets([pos_ds, neg_ds], weights=weights)
+        return resampled_ds.batch(batch_size)
+
